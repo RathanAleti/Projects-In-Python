7fce1a003fa56c12732f1b41a8e9539d368d0900
RathanAleti <145594798+RathanAleti@users.noreply.github.com>
2025-05-20 09:26:17 -0500

    Updated the hangman project
    Changed the Code to improve the user experience

--- The Hangman project.py
+++ The Hangman project.py
@@ -1,65 +1,17 @@
 import random
-images = [r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import hangman_art
+import hangman_words
 
-# List words as city names 
-words = ["barcelona","newyork","kansas","hyderabad"]
-Random_word = random.choice(words)#Picking a random word from the words list 
-print (Random_word)
+# Importing the Hangman words file and assign it to words 
+words = hangman_words.word_list
+images = hangman_art
+
+#Importing the logo from hangman_art.py and print it at the start of the game
+print(hangman_art.logo)
+
+#Picking a random word from the words list
+Random_word = random.choice(words)
+print(Random_word)
 
 # Declaring the placeholder variable empty because it can go through the word and generate
 # as many blanks the rand word as 
@@ -69,39 +21,51 @@ word_len = len(Random_word)
 #Using For loop to generate as many blanks as letter in word
 for x in range(word_len):
     Place_holder +=" _" 
-print(Place_holder)
+print("Word to guess: " + Place_holder)
+ 
+game_over = False #setting game over variable as False 
+lives = 6 #Declaring lives variable and assign the value of 6
+guess_letters = []# Creating a list called guess_letters to keep track of the guessed letter in the display
 
-
-#setting game over variable as False 
-game_over = False
-lives = 6
-# Creating a list called guess_letters to keep track of the guessed letter in the display
-guess_letters = []
 # Using while loop to ask the user to guess the correct word till there lives == 0
 while not game_over:
-    display = ""
+    
+    print(f"****************************<{lives}>/6 LIVES LEFT****************************")
     # Asking the user to guess a word
-    guess = input("Guess a [letter to save a man: ").lower()
+    guess = input("Guess a letter to save a man: ").lower()
+    
+    # If the user guessed the letter twice , letting them know and also they don't lose there lives
+    if guess in guess_letters:
+        print(f"You've already guessed {guess}")
+    
+    display = ""
     #Using for loop to go through evert letter in Random_word variable
+
     for letters in Random_word:
-        if letters == guess: #if user guess the letter correct the letter will add to the display
+        #if user guess the letter correct the letter will add to the display
+        if letters == guess: 
             display += letters
             guess_letters.append(guess)
-        elif letters in guess_letters: # Checking if the letters are matching with the guess_letters values
+        # Checking if the letters are matching with the guess_letters values
+        elif letters in guess_letters: 
             display += letters # If they match it will keep track of the previous guess letter by user
+
         else :
             display += " _"
 
-    print(display)
+    print("Word to guess: " + display)
+    
     if guess not in Random_word:
         lives -=1
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
         if lives == 0:
             game_over = True
-            print("You lose")
+            print(f"*********************** The Word is {Random_word} YOU LOSE**********************")
 
-    elif " _" not in display:
+    if " _" not in display:
         game_over = True
         print("You win.")
-
+    
+    images = hangman_art.stages
     print(images[lives])
 
